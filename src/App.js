6eb6a01b7245b96fd02e7787d22e10ae4b69031d
John Baxendale <siftah@Johns-Air.siftah.net>
2023-07-06 23:10:51 +0200

    Alpha version of reading the data and populating menus

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import { useState } from "react";
 import * as XLSX from "xlsx";
+import {parseDate} from '@internationalized/date';
 
-import './App.css';
+import { ComboBox, Item, Section, DatePicker, Button, defaultTheme, Provider, Grid, View, TextField} from '@adobe/react-spectrum';
+
+
 
 function App() {
-
-  const [data, setData] = useState([]);
+  let [data, setData] = useState([]);
+  const [partnersData, setPartnersData] = useState([]);
+  const [productsData, setProductsData] = useState([]);
+  const [metricsData, setMetricsData] = useState([]);
 
   const handleFileUpload = (e) => {
     const reader = new FileReader();
@@ -13,31 +18,89 @@ function App() {
     reader.onload = (e) => {
       const data = e.target.result;
       const workbook = XLSX.read(data, { type: "binary" });
-      const sheetName = workbook.SheetNames[0];
-      const sheet = workbook.Sheets[sheetName];
-      const parsedData = XLSX.utils.sheet_to_json(sheet);
+      const parsedData = XLSX.utils.sheet_to_json(workbook.Sheets["Data Entry"]);
+      const partnersData = XLSX.utils.sheet_to_json(workbook.Sheets["Entities"]);
+      const metricsData = XLSX.utils.sheet_to_json(workbook.Sheets["Metrics"]);
+      const productsData = XLSX.utils.sheet_to_json(workbook.Sheets["Products"]);
+      
+
       setData(parsedData);
+      setPartnersData(partnersData);
+      setProductsData(productsData);
+      setMetricsData(metricsData);
+
+      parsedData.sort(function(a,b){
+                // Turn your strings into dates, and then subtract them
+                // to get a value that is either negative, positive, or zero.
+                return new Date(b.date) - new Date(a.date);
+              });
+              //array.shift();
     };
   }
 
-  return (
-    <div className="App">
+  let products = [];
+  let partners = [];
 
-      <input 
+function makeDropdownProducts (value, index, array){
+  products.push({id: index, name: value["Products"]});
+}
+
+function makeDropdownPartners (value, index, array){
+  partners.push({id: index, name: value["Entities"]});
+}
+
+  productsData.forEach(makeDropdownProducts);
+  partnersData.forEach(makeDropdownPartners);
+
+  products.sort();
+  partners.sort();
+
+
+  return (
+ 
+    <div className="App">
+    <Provider theme={defaultTheme}>
+
+    <input 
         type="file" 
         accept=".xlsx, .xls" 
         onChange={handleFileUpload} 
-      />
+    />
+
+     <ComboBox
+        label="Partner"
+        defaultItems={partners}
+        defaultInputValue={""}>
+        {item => <Item>{item.name}</Item>}
+      </ComboBox>
+
+      <ComboBox
+        label="Product"
+        defaultItems={products}
+        defaultInputValue={""}>
+        {item => <Item>{item.name}</Item>}
+      </ComboBox>
+
+    <DatePicker 
+      label="Start Date"
+      defaultValue={parseDate('2023-05-01')}
+    />
+
+    <DatePicker 
+      label="End Date"
+      defaultValue={parseDate('2023-07-01')}
+    />
+    </Provider>
 
       {data.length > 0 && (
         <table className="table">
           <thead>
             <tr>
-              {Object.keys(data[0]).map((key) => (
+              {Object.keys(data[1]).map((key) => (
                 <th key={key}>{key}</th>
               ))}
             </tr>
-          </thead>
+          </thead>   
           <tbody>
             {data.map((row, index) => (
               <tr key={index}>
@@ -48,10 +111,8 @@ function App() {
             ))}
           </tbody>
         </table>
-      )}
 
-      <br /><br />
-      ... webstylepress ...
+      )}
     </div>
   );
 }
